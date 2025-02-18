f7152d57847165eb78fbe52e9a4b0f44a8928746
drive station <drivestation@3636.team>
2025-02-18 14:51:08 -0800

    feat: better climb code

--- src/main/kotlin/com/frcteam3636/frc2025/subsystems/climb/Climb.kt
+++ src/main/kotlin/com/frcteam3636/frc2025/subsystems/climb/Climb.kt
@@ -1,13 +1,15 @@
 package com.frcteam3636.frc2025.subsystems.climb
 
 import com.frcteam3636.frc2025.Robot
+import edu.wpi.first.units.Units.Volts
+import edu.wpi.first.units.measure.Voltage
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Subsystem
 import org.littletonrobotics.junction.Logger
 
 object Climb : Subsystem {
     private val io: ClimbIO = when (Robot.model) {
-        Robot.Model.SIMULATION -> TODO()
+        Robot.Model.SIMULATION -> ClimbIOSim()
         Robot.Model.COMPETITION -> ClimbIOReal()
         Robot.Model.PROTOTYPE -> TODO()
     }
@@ -21,20 +23,28 @@ object Climb : Subsystem {
 
     fun up(): Command = startEnd(
         {
-            io.setSpeed(0.50)
+            io.setVoltage(Volts.of(12.0))
         },
         {
-            io.setSpeed(0.0)
+            io.setVoltage(Volts.of(0.0))
         }
     )
 
     fun down(): Command = startEnd(
         {
-            io.setSpeed(-0.50)
+            io.setVoltage(Volts.of(6.0))
         },
         {
-            io.setSpeed(0.0)
+            io.setVoltage(Volts.of(0.0))
         }
     )
 
+}
+
+class ClimbIOSim : ClimbIO {
+    override fun setSpeed(percent: Double) {}
+
+    override fun setVoltage(voltage: Voltage) {}
+
+    override fun updateInputs(inputs: ClimbInputs) {}
 }
--- src/main/kotlin/com/frcteam3636/frc2025/subsystems/climb/ClimbIO.kt
+++ src/main/kotlin/com/frcteam3636/frc2025/subsystems/climb/ClimbIO.kt
@@ -4,7 +4,9 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.signals.NeutralModeValue
 import com.frcteam3636.frc2025.CTREDeviceId
 import com.frcteam3636.frc2025.TalonFX
+import com.frcteam3636.frc2025.utils.math.volts
 import edu.wpi.first.units.Units.Amps
+import edu.wpi.first.units.measure.Voltage
 import org.team9432.annotation.Logged
 
 @Logged
@@ -14,6 +16,7 @@ open class ClimbInputs {
 
 interface ClimbIO {
     fun setSpeed(percent: Double)
+    fun setVoltage(voltage: Voltage)
     fun updateInputs(inputs: ClimbInputs)
 }
 
@@ -31,6 +34,11 @@ class ClimbIOReal : ClimbIO {
     override fun setSpeed(percent: Double) {
         assert(percent in -1.0..1.0)
         climbMotor.set(percent)
+    }
+
+    override fun setVoltage(voltage: Voltage) {
+        assert(voltage.volts in -12.0..12.0)
+        climbMotor.setVoltage(voltage.volts)
     }
 
     override fun updateInputs(inputs: ClimbInputs) {
