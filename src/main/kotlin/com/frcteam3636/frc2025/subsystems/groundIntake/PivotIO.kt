863e3d1e5365d8d638de402114605dfd5b1fce0d
Tai Morioka <thetaimorioka@gmail.com>
2025-03-19 20:03:53 -0700

    Worked on it for a while more, need to leave so not done.

--- src/main/kotlin/com/frcteam3636/frc2025/subsystems/groundIntake/PivotIO.kt
+++ src/main/kotlin/com/frcteam3636/frc2025/subsystems/groundIntake/PivotIO.kt
@@ -1,9 +1,18 @@
 package com.frcteam3636.frc2025.subsystems.groundIntake
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration
+import com.ctre.phoenix6.signals.InvertedValue
+import com.ctre.phoenix6.signals.NeutralModeValue
 import com.frcteam3636.frc2025.CTREDeviceId
 import com.frcteam3636.frc2025.TalonFX
+import com.frcteam3636.frc2025.utils.math.MotorFFGains
+import com.frcteam3636.frc2025.utils.math.PIDGains
+import com.frcteam3636.frc2025.utils.math.inRotationsPerSecond
+import com.frcteam3636.frc2025.utils.math.*
+import com.frcteam3636.frc2025.utils.math.toAngular
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.units.Units.*
+import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.units.measure.Voltage
 
 open class PivotInputs{
@@ -25,19 +34,80 @@ interface PivotIO{
     fun setMotorVoltage(voltage: Voltage)
     fun setTargetPostion(postion: Rotation2d)
 }
+
 class PivotIOReal: PivotIO{
-    private val leftMotor = TalonFX(CTREDeviceId.LeftPivotMotor).apply { }
+
+    private val config = TalonFXConfiguration().apply {
+        MotorOutput.apply {
+            Inverted = InvertedValue.Clockwise_Positive
+            NeutralMode = NeutralModeValue.Brake
+        }
+
+        Slot0.apply {
+            pidGains = PID_GAINS
+            motorFFGains = FF_GAINS
+            kG = GRAVITY_GAIN
+        }
+
+        Feedback.apply {
+            SensorToMechanismRatio = SENSOR_TO_MECHANISM_GEAR_RATIO
+        }
+
+        MotionMagic.apply {
+            MotionMagicCruiseVelocity = PROFILE_VELOCITY.inRotationsPerSecond()
+            MotionMagicAcceleration = PROFILE_ACCELERATION
+            MotionMagicJerk = PROFILE_JERK
+        }
+
+        CurrentLimits.apply {
+            StatorCurrentLimitEnable = true
+            StatorCurrentLimit = 0.0
+
+            SupplyCurrentLimitEnable = true
+            SupplyCurrentLimit = 0.0
+        }
+    }
+
+    private val leftMotor = TalonFX(CTREDeviceId.LeftPivotMotor).apply {
+        configurator.apply { config }
+    }
+
+    private val rightMotor = TalonFX(CTREDeviceId.RightPivotMotor).apply {
+        configurator.apply { config }
+    }
+
+    override fun updateInputs(inputs: PivotInputs) {
+
+        inputs.leftMotorCurrent = leftMotor.supplyCurrent.value
+        inputs.leftMotorVelocity = leftMotor.velocity.value
+
+        inputs.rightMotorCurrent = rightMotor.supplyCurrent.value
+        inputs.rightMotorVelocity = rightMotor.velocity.value
+
+        inputs.leftPostion = leftMotor.position as Angle
+        inputs.rightPostion = rightMotor.position as Angle
+
+        inputs.absoluteEncoderConnected = false
+
+    }
 
     override fun setTargetPostion(postion: Rotation2d) {
         TODO("Not yet implemented")
     }
 
-    override fun updateInputs(inputs: PivotInputs) {
+    override fun setMotorVoltage(voltage: Voltage) {
         TODO("Not yet implemented")
     }
 
-    override fun setMotorVoltage(voltage: Voltage) {
-        TODO("Not yet implemented")
+    internal companion object Constants {
+        val SPOOL_RADIUS = 0.0.inches
+        private val SENSOR_TO_MECHANISM_GEAR_RATIO = 0.0
+        private val PID_GAINS = PIDGains(0.0, 0.0, 0.0)
+        private val FF_GAINS = MotorFFGains(0.0, 0.0, 0.0)
+        private const val GRAVITY_GAIN = 0.0
+        private val PROFILE_ACCELERATION = 0.0
+        private const val PROFILE_JERK = 0.0
+        private val PROFILE_VELOCITY = 200.inchesPerSecond.toAngular(SPOOL_RADIUS)
     }
 }
 
--- src/main/kotlin/com/frcteam3636/frc2025/subsystems/groundIntake/RollerIO.kt
+++ src/main/kotlin/com/frcteam3636/frc2025/subsystems/groundIntake/RollerIO.kt
@@ -1,17 +1,17 @@
 package com.frcteam3636.frc2025.subsystems.groundIntake
 import com.frcteam3636.frc2025.REVMotorControllerId
 import com.frcteam3636.frc2025.SparkFlex
-import com.frcteam3636.frc2025.SparkMax
 import com.frcteam3636.frc2025.utils.math.amps
 import com.frcteam3636.frc2025.utils.math.radiansPerSecond
+import com.revrobotics.spark.SparkBase
 import com.revrobotics.spark.SparkLowLevel
 import com.revrobotics.spark.config.SparkBaseConfig
 import com.revrobotics.spark.config.SparkFlexConfig
 import edu.wpi.first.units.Units.*
 
 open class RollerInputs{
-    var velocity = RotationsPerSecond.zero()
-    var current = Amps.zero()
+    var velocity = RotationsPerSecond.zero()!!
+    var current = Amps.zero()!!
 }
 
 interface RollerIO{
@@ -20,13 +20,15 @@ interface RollerIO{
 }
 
 class RollerIOReal : RollerIO {
-    private var rollerMotor = SparkFlex(REVMotorControllerId.GroundIntakeRollerMotor, SparkLowLevel.MotorType.kBrushless).apply{
-        val config = SparkFlexConfig().apply{
-            idleMode(SparkBaseConfig.IdleMode.kBrake)
-        }
 
-        this.configure(config)
+    private var rollerMotor = SparkFlex(REVMotorControllerId.GroundIntakeRollerMotor, SparkLowLevel.MotorType.kBrushless).apply {
+        configure(
+            SparkFlexConfig().apply{ idleMode(SparkBaseConfig.IdleMode.kBrake) },
+            SparkBase.ResetMode.kResetSafeParameters,
+            SparkBase.PersistMode.kPersistParameters,
+        )
     }
+
     override fun updateInputs(inputs: RollerInputs) {
         inputs.velocity = rollerMotor.encoder.velocity.radiansPerSecond
         inputs.current = rollerMotor.outputCurrent.amps
@@ -39,6 +41,13 @@ class RollerIOReal : RollerIO {
 
 }
 
-interface RollerIOSim : RollerIO {
-    // TODO()
+class RollerIOSim : RollerIO {
+
+    override fun setSpeed(percentage: Double) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateInputs(inputs: RollerInputs) {
+        TODO("Not yet implemented")
+    }
 }
